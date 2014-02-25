8ef225ad43f496971164dbeb92167ed999db3838
Kevin Gilmore <kevmisterio@gmail.com>
2014-02-25 16:57:16 -0500

    Addition of hosting

---
+++ www/canvas/engine.js
@@ -0,0 +1,17 @@
+/*
+Engine objects
+  viewports
+    resolution
+      fixed
+      dynamic
+    scale
+      stretch
+      maintain
+       center
+  scenes
+    sprites
+    objects
+*/
+var engine = function(){
+
+};
